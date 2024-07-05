01dcbe10d27f247fac63747eba5304cf3a87456a
Eduarda Perboire <eduarda.perboire@avantia.com.br>
2024-07-05 17:09:14 -0300

    update validation

--- dguard-validation.py
+++ dguard-validation.py
@@ -4,23 +4,21 @@ app = Flask(__name__)
 
 @app.route('/motion-detected', methods=['POST'])
 def motion_detected():
-    data = request.get_json(silent=True)
-    if data:
-        print("Webhook received JSON data:", data)
-        print('Motion detected data received:', data)
-        return jsonify({"status": "success", "message": "Data received"}), 200
-    else:
-        # Se não for JSON, tente obter dados brutos (texto ou outros formatos)
-        data = request.data
-        print("Webhook received raw data:", data)
-    # try:
-    #     data = request.get_json()
-    #     if data is None:
-    #         raise ValueError("No JSON data received")
+    try:
+        data = request.get_json(silent=True)
+        if data:
+            print("Webhook received JSON data:", data)
+            print('Motion detected data received:', data)
+            return jsonify({"status": "success", "message": "Data received"}), 200
+        else:
+            # Se não for JSON, tente obter dados brutos (texto ou outros formatos)
+            data = request.data
+            print("Webhook received raw data:", data)
+            return jsonify({"status": "error", "message": str(e)}), 400
         
-    # except Exception as e:
-    #     print(f"Error processing request: {e}")
-    #     return jsonify({"status": "error", "message": str(e)}), 400
+    except Exception as e:
+        print(f"Error processing request: {e}")
+        return jsonify({"status": "error", "message": str(e)}), 400
 
 if __name__ == '__main__':
     print("executando...")
